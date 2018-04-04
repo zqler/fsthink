3fa3d95a435507a8b46d3d88de02306a1d554d9a
Your Name <434561406@qq.com>
2018-04-04 17:35:28 +0800

    首页添加用户列表

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,24 +1,51 @@
 <template>
   <div class="home">
    <h3>这是一个首页</h3>
-
+      <el-table
+      :data="users"
+      :row-class-name="tableRowClassName"
+      style="width: 80%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
 export default {
   name: "home",
   data() {
-    return {};
+    return {
+      users: []
+    };
   },
   created() {
     var $this = this;
     const user = {};
     user.name = "zq";
     this.$http.get("/usersList", user).then(data => {
-      console.log(data);
+      if (data.errcode == 0) {
+        this.users = data.resultData.list;
+        console.log(this.users);
+      }
     });
   },
-  methods: {}
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
+      return rowIndex / 2 === 0 ? "warning-row" : "success-row";
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -27,5 +54,12 @@ export default {
   h3 {
     color: #666;
   }
+}
+.el-table .warning-row {
+  background: oldlace !important;
+}
+
+.el-table .success-row {
+  background: #f0f9eb !important;
 }
 </style>
