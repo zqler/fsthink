f022290520bb6686b20af3b026fc9d736e70ddc0
Your Name <434561406@qq.com>
2018-04-08 17:27:39 +0800

    首页表格

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="home">
    <h3>这是一个首页</h3>
-      <el-table
-      :data="users"
-      :row-class-name="tableRowClassName"
-      style="width: 80%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
+       <el-table
+    :data="users"
+    style="width: 100%"
+    :row-class-name="tableRowClassName">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+    <el-table-column
+      fixed="right"
+      label="操作"
+      width="150">
+      <template slot-scope="scope">
+        <el-button  @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button  @click="edit(scope.row)" type="text" size="small">编辑</el-button>
+        <el-button @click="del(scope.$index,users)" type="text" size="small" >删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
   </div>
 </template>
 <script>
 export default {
-  name: "home",
   data() {
     return {
       users: []
@@ -43,23 +52,31 @@ export default {
   },
   methods: {
     tableRowClassName({ row, rowIndex }) {
-      return rowIndex / 2 === 0 ? "warning-row" : "success-row";
+      if (rowIndex === 1) {
+        return "warning-row";
+      } else if (rowIndex === 3) {
+        return "success-row";
+      }
+      return "";
+    },
+    handleClick(row) {
+      console.log(row.date);
+    },
+    edit(row) {
+      console.log(row);
+    },
+    del(index, rows) {
+      rows.splice(index, 1);
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-.home {
-  margin: 0 auto;
-  h3 {
-    color: #666;
-  }
-}
 .el-table .warning-row {
-  background: oldlace !important;
+  background: oldlace;
 }
 
 .el-table .success-row {
-  background: #f0f9eb !important;
+  background: #f0f9eb;
 }
 </style>
